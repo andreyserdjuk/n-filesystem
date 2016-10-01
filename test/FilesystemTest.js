282b940c6fdb6d716ff7f4a303de2a4d5f400d6f
andreyserdjuk <ase6120@gmail.com>
2016-10-01 19:40:57 +0300

    tmp dir slash added

--- test/FilesystemTest.js
+++ test/FilesystemTest.js
@@ -7,28 +7,28 @@ const os = require('os');
 let filesystem = new Filesystem_1.Filesystem();
 let TMPDIR = os.tmpdir();
 describe('Filesystem', function () {
-    var cleanup = () => { cp.exec('rm -rf ' + TMPDIR + 'a'); };
+    var cleanup = () => { cp.exec('rm -rf ' + TMPDIR + '/a'); };
     before((done) => {
         cleanup();
-        fs.writeFileSync(TMPDIR + 'test_file1', '1');
-        fs.writeFileSync(TMPDIR + 'test_file2', '1');
+        fs.writeFileSync(TMPDIR + '/test_file1', '1');
+        fs.writeFileSync(TMPDIR + '/test_file2', '1');
         done();
     });
     after((done) => {
         cleanup();
         try {
-            fs.unlinkSync(TMPDIR + 'test_file1');
+            fs.unlinkSync(TMPDIR + '/test_file1');
         }
         catch (e) { }
         try {
-            fs.unlinkSync(TMPDIR + 'test_file2');
+            fs.unlinkSync(TMPDIR + '/test_file2');
         }
         catch (e) { }
         done();
     });
     it('mkdirSync: create nested set of dirs', function () {
         filesystem.mkdirSync(['a', 'b', 'c'], 0o777, TMPDIR);
-        let stat = fs.statSync(TMPDIR + 'a/b/c');
+        let stat = fs.statSync(TMPDIR + '/a/b/c');
         assert.ok(stat.isDirectory());
     });
     //      S_IFMT     0170000   bit mask for the file type bit field
@@ -40,22 +40,22 @@ describe('Filesystem', function () {
     //      S_IFCHR    0020000   character device
     //      S_IFIFO    0010000   FIFO
     it('mkdirSync: created dirs should have 777 permissions', () => {
-        let stat = fs.statSync(TMPDIR + 'a/b/c');
+        let stat = fs.statSync(TMPDIR + '/a/b/c');
         assert.equal(stat.mode.toString(8), '40777');
     });
     it('copySync: existence of copied file', () => {
-        filesystem.copySync(TMPDIR + 'test_file1', TMPDIR + 'test_file2');
-        assert.ok(fs.statSync(TMPDIR + 'test_file1').isFile());
+        filesystem.copySync(TMPDIR + '/test_file1', TMPDIR + '/test_file2');
+        assert.ok(fs.statSync(TMPDIR + '/test_file1').isFile());
     });
     it('existsSync: check list of files, single file', () => {
-        let files = [TMPDIR + 'test_file1', TMPDIR + 'test_file2'];
+        let files = [TMPDIR + '/test_file1', TMPDIR + '/test_file2'];
         let dirs = [TMPDIR];
         assert.ok(filesystem.existsSync(dirs));
         assert.ok(filesystem.existsSync(files));
-        assert.notEqual(true, filesystem.existsSync(TMPDIR + 'asldfjkalsd'));
+        assert.notEqual(true, filesystem.existsSync(TMPDIR + '/asldfjkalsd'));
     });
     it('touchSync: array of files - atime, mtime', () => {
-        let files = [TMPDIR + 'touch_file1', TMPDIR + 'touch_file2'];
+        let files = [TMPDIR + '/touch_file1', TMPDIR + '/touch_file2'];
         after((done) => {
             for (let file of files)
                 fs.unlink(file);
@@ -71,22 +71,22 @@ describe('Filesystem', function () {
         }
     });
     it('removeSync: remove list of files', () => {
-        filesystem.removeSync([TMPDIR + 'test_file1', TMPDIR + 'test_file2']);
-        assert.ok(!fs.existsSync(TMPDIR + 'test_file1'));
-        assert.ok(!fs.existsSync(TMPDIR + 'test_file2'));
+        filesystem.removeSync([TMPDIR + '/test_file1', TMPDIR + '/test_file2']);
+        assert.ok(!fs.existsSync(TMPDIR + '/test_file1'));
+        assert.ok(!fs.existsSync(TMPDIR + '/test_file2'));
     });
     it('chmodSync', () => {
-        filesystem.chmodSync(TMPDIR + 'a', 0o755);
-        assert.equal(fs.statSync(TMPDIR + 'a').mode.toString(8), '40755');
-        assert.equal(fs.statSync(TMPDIR + 'a/b').mode.toString(8), '40777');
-        assert.equal(fs.statSync(TMPDIR + 'a/b/c').mode.toString(8), '40777');
-        filesystem.chmodSync(TMPDIR + 'a', 0o755, 0o000, true);
-        assert.equal(fs.statSync(TMPDIR + 'a').mode.toString(8), '40755');
-        assert.equal(fs.statSync(TMPDIR + 'a/b').mode.toString(8), '40755');
-        assert.equal(fs.statSync(TMPDIR + 'a/b/c').mode.toString(8), '40755');
+        filesystem.chmodSync(TMPDIR + '/a', 0o755);
+        assert.equal(fs.statSync(TMPDIR + '/a').mode.toString(8), '40755');
+        assert.equal(fs.statSync(TMPDIR + '/a/b').mode.toString(8), '40777');
+        assert.equal(fs.statSync(TMPDIR + '/a/b/c').mode.toString(8), '40777');
+        filesystem.chmodSync(TMPDIR + '/a', 0o755, 0o000, true);
+        assert.equal(fs.statSync(TMPDIR + '/a').mode.toString(8), '40755');
+        assert.equal(fs.statSync(TMPDIR + '/a/b').mode.toString(8), '40755');
+        assert.equal(fs.statSync(TMPDIR + '/a/b/c').mode.toString(8), '40755');
     });
     it('chownSync', () => {
-        filesystem.chownSync(TMPDIR + 'a', 999, true);
-        assert.equal(fs.statSync(TMPDIR + '/a').uid, 999);
+        filesystem.chownSync(TMPDIR + '/a', 999, true);
+        assert.equal(fs.statSync(TMPDIR + '//a').uid, 999);
     });
 });
